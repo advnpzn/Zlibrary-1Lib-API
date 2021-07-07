db22cd13a2c77483797495fd2166eace7ab5ffba
adenosinetp10 <adenosinetp10@gmail.com>
2021-07-07 19:05:29 +0530

    initialized scrapping using bs4

--- zlib_api/zlib_scrap.py
+++ zlib_api/zlib_scrap.py
@@ -1,10 +1,27 @@
 
+from os import name
 from typing import Union
 from bs4 import BeautifulSoup
 import requests
-from typing import Union
+from fake_headers import Headers
 
-URL = "https://1lib.in/"
+
+URL = "https://1lib.in"
+
+COLUMN = [
+    "id",
+    "name",
+    "thumbnail_url",
+    "publisher",
+    "publisher_url",
+    "author",
+    "author_url",
+    "book_url",
+    "year",
+    "language",
+    "extension",
+    "size",
+]
 
 class Scrap:
 
@@ -32,7 +49,7 @@ class Scrap:
                 EXT_STR = EXT_STR+f"&extensions%5B{i}%5D={ext[i]}"
         elif type(ext) is str:
             EXT_STR = f"&extensions%5B0%5D={ext}"
-        SEARCH_URL = f"{URL}s/{query.replace(' ','%20')}/?"
+        SEARCH_URL = f"{URL}/s/{query.replace(' ','%20')}/?"
         if exact_matching == True:
             SEARCH_URL = SEARCH_URL+"e=1"
         else:
@@ -61,7 +78,7 @@ class Scrap:
         Returns the input url with sorted search.
 
 
-            _Parameters:_
+            Parameters:
                     url (str): The object Scrap().search(*args, *kwargs)
                     order (str): The sort order
             * popular - Sort by popularity (desc)
@@ -69,7 +86,7 @@ class Scrap:
             * date - Sort by Recently added (desc)
             * titleA - Sort by Title(A-Z)
             * title - Sort by Title(Z-A)
-            * **year - Sort by Year of publication (desc)
+            * year - Sort by Year of publication (desc)
             * filsize - Sort by File size (desc)
             * filesizeA - Sort by File size (asc)
             Returns:
@@ -78,17 +95,100 @@ class Scrap:
         return f"{url}&order={order}"
 
 
-    def scrap(self, url:str) -> str:
+    def scrap(self, url:str):
         """
         """
+        #header = Headers(browser="chrome", os="win", headers=True)
         res = requests.get(url=url).text
         soup = BeautifulSoup(markup=res, features='lxml')
-        divs = soup.find_all(name='div', attrs={'class': 'resItemBox resItemBoxBooks exactMatch'})
-        return divs
+    
+        names = soup.find_all(name="a", attrs={"style": "text-decoration: underline;"})
+        names = [i.text for i in names]
+        thumb_img = soup.find_all(name="img", attrs={"class": "cover lazy"})
+        thumb_img = [i.get('data-src') for i in thumb_img]
+        book_url = soup.find_all(name="h3", attrs={"itemprop": "name"})
+        book_url = [f"{URL}{book_url[i].a.get('href')}" for i in range(len(book_url))]
+        id = [i.split('/')[5] for i in book_url]
+        
+        publisher = soup.find_all(name="div", attrs={"title": "Publisher"})
+        publisher_url = [f"{URL}{i.find(name='a').get('href')}" for i in publisher]
+        publisher = [i.find(name = "a").text for i in publisher]
+        author_scrap = soup.find_all(name="div", attrs={"class": "authors"})
+        author_url = []
+        authors = []
+        for i in author_scrap:
+            authors_dict = {}
+            author_url_dict = {}
+            auth_per_book = i.find_all("a", {"title": "Find all the author's books"})
+            if len(auth_per_book) > 1:
+                for i in range(len(auth_per_book)):
+                    authors_dict[i] = auth_per_book[i].text
+                    author_url_dict[i] = auth_per_book[i].get("href")
+                author_url.append(author_url_dict)
+                authors.append(authors_dict)
+            else:
+                author_url.append(auth_per_book[0].get("href"))
+                authors.append(auth_per_book[0].text)
+        #print(len(author_url))
+        #print(len(authors))
+        year = soup.find_all(name="div", attrs={"class": "bookProperty property_year"})
+        year = [i.find("div", {"class": "property_value"}).text for i in year]
+        lang = soup.find_all(name="div", attrs={"class": "bookProperty property_language"})
+        lang = [i.find("div", {"class": "property_value"}).text for i in lang]
+        size_n_ext = soup.find_all(name="div", attrs={"class": "bookProperty property__file"})
+        size_n_ext = [i.find("div", {"class": "property_value"}).text for i in size_n_ext]
+        size = []
+        extenstion = []
+        for i in size_n_ext:
+            size_ext = i.split(', ')
+            size.append(size_ext[1])
+            extenstion.append(size_ext[0])
+        print(f"id {len(id)}")
+        print(f"names {len(names)}")
+        print(f"thumb_img {len(thumb_img)}")
+        print(f"book_url {len(book_url)}")
+        print(f"publisher_url {len(publisher_url)}")
+        print(f"publisher {len(publisher)}")
+        print(f"author_url {len(author_url)}")
+        print(f"authors {len(authors)}")
+        print(f"year {len(year)}")
+        print(f"lang {len(lang)}")
+        print(f"size {len(size)}")
+        print(f"ext {len(extenstion)}")
+        #return_list = []
+        '''
+        for i in range(len(id)):
+            return_list.append(
+                {
+                    "id": id[i],
+                    "name": names[i],
+                    "thumbnail_url": thumb_img[i],
+                    "publisher": publisher[i],
+                    "publisher_url": publisher_url[i],
+                    "author": author[i],
+                    "author_url": author_url[i],
+                    "book_url": book_url[i],
+                    "year": year[i],
+                    "language": lang[i],
+                    "extension": extenstion[i],
+                    "size": size[i],
+                }
+            )
+        return return_list'''
+
+        
+
+
+
+    def download_url(self, url: str) -> str:
+        book_res = requests.get(url=url).text
+        book_soup = BeautifulSoup(markup=book_res, features="lxml")
+        dl_url = f"{URL}{book_soup.find(name='a', attrs={'class': 'btn btn-primary dlButton addDownloadedBook'}).get('href')}"
+        return dl_url
 
 
 
 a = Scrap()
-b = a.search(query="automate the boring stuff with python", lang='english', ext='pdf')
+b = a.search(query="automate the boring stuff with python", lang=['english'], ext='pdf')
 c = a.sort_search(url=b, order='filesize')
 print(a.scrap(c))
